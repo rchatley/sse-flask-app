10cb56cd33636b44ace3ff4db634467e31933149
Robert Chatley <rbc@imperial.ac.uk>
2024-08-23 16:26:10 +0100

    Remove unused import.

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -15,7 +13,6 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
-
 
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)), debug=False)
